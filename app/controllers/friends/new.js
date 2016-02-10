9e63234fe7ff2b1d297e6bd12b6a692fbca3b22b
Connie Emerson <constance.emerson@alphasights.com>
2016-02-09 22:01:43 -0500

    Initial commit

---
+++ app/controllers/friends/new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import FriendsBaseController from './base';
+
+export default FriendsBaseController.extend({
+  actions: {
+    cancel: function() {
+      this.transitionToRoute('friends');
+
+      return false;
+    }
+  }
+});
---
+++ app/routes/friends.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    save: function() {
+    console.log('+--- save action bubbled up to friends route');
+    return false;
+    },
+    cancel: function() {
+      console.log('+--- cancel action bubbled up to friends route');
+      return false;
+      },
+    delete: function(friend) {
+      var _this = this;
+
+      friend.destroyRecord().then(function() {
+          _this.transitionTo('friends.index');
+      });
+    }
+  }
+});
